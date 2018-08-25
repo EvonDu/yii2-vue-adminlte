ce8ef80f2d001d6584d9d89d309eaf5fff6d498b
EvonDu <298550274@qq.com>
2018-08-25 17:56:27 +0800

    变更_form的模板函数为匿名函数(闭包)

--- src/template/views/_form.php
+++ src/template/views/_form.php
@@ -23,7 +23,7 @@ use vuelte\widgets\ActiveElementForm;
 /* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
 /* @var $form yii\widgets\ActiveForm */
 ?>
-<?= "<?php " ?> function template($model){ ?>
+<?= "<?php " ?>$template = function($model){ ?>
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-form">
 
     <?= "<?php " ?>ActiveElementForm::begin(["options"=>[
@@ -47,7 +47,7 @@ use vuelte\widgets\ActiveElementForm;
 
 <script>
     Vue.component('model-form', {
-        template: `<?= "<?=" ?> template($model); ?>`,
+        template: `<?= "<?=" ?> $template($model); ?>`,
         props:{
             'data':{ type: Object, default: function(){ return {}; }}
         },
