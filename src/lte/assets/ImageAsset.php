6702005d00cf63a5354c1de11e9a1fcee4ff0a0e
EvonDu <298550274@qq.com>
2018-12-28 17:15:13 +0800

    添加图片资源类

---
+++ src/lte/assets/ImageAsset.php
@@ -0,0 +1,33 @@
+<?php
+namespace vuelte\lte\assets;
+
+use yii\web\AssetBundle;
+use yii\web\View;
+
+/**
+ * Class ImageAsset
+ * @package vuelte\lte\assets
+ */
+class ImageAsset extends AssetBundle {
+    /**
+     * @var string
+     */
+    public $sourcePath = __DIR__.'/../static/image';
+
+    /**
+     * @param View $view
+     * @param $name
+     * @return string
+     */
+    static function get(View $view, $name){
+        //加载资源包
+        if(!$view->getAssetManager()->getBundle(self::className()))
+            self::register($view);
+
+        //获取基本地址
+        $asset_url=$view->getAssetManager()->getBundle(self::className())->baseUrl;
+
+        //返回访问地址
+        return "$asset_url/$name";
+    }
+}
